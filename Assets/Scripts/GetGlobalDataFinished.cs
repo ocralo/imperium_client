444b841cec5ddece4caa9af9ec60f5777bfc151a
ocralo <ocralo@gmail.com>
2021-03-21 03:14:38 -0500

    update

---
+++ Assets/Scripts/GetGlobalDataFinished.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetGlobalDataFinished : MonoBehaviour
+{
+    public GameObject globalDataObj;
+    public TextMeshProUGUI textTimer;
+    public TextMeshProUGUI textPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        globalDataObj = GameObject.Find("globalData");
+        textTimer.text = globalDataObj.GetComponent<GlobalData>().time +"";
+        textPoint.text = globalDataObj.GetComponent<GlobalData>().point +"";
+    }
+}
